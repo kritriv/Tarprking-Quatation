9ae976a5afc4e5451fbeebd22e8688c3cc75de11
VISHAL SARAIWAL <vishalsaraiwal68@gmail.com>
2024-02-15 10:53:10 +0530

    auth route fix

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -5,8 +5,8 @@ const { Register, Login, Logout } = require('../controllers/Auth');
 const { RegisterSchema, LoginSchema } = require('../validators/Schemas');
 const validate = require('../validators/validate');
 
-router.post('auth/register', validate(RegisterSchema), Register);
-router.post('auth/login', validate(LoginSchema), Login);
-router.post('auth/logout', Logout);
+router.post('/auth/register', validate(RegisterSchema), Register);
+router.post('/auth/login', validate(LoginSchema), Login);
+router.post('/auth/logout', Logout);
 
 module.exports = router;
